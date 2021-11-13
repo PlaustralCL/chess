dc080234f6e91b9c2c106f80f74b7625474d12fa
PlaustralCL <plaustralcl@gmail.com>
2021-11-13 17:00:35 -0500

    Validate black pawn moves

---
+++ lib/black_pawn_move.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+require_relative "piece_move"
+
+# Validates that a requested pawn move is allowed
+class BlackPawnMove < PieceMove
+  def basic_rules?
+    basic_move? || capture_allowed?
+  end
+
+  # Only applies to straight moves, not captures
+  def clear_path?
+    return true unless same_column?
+
+    paths = [find_file]
+    target_row = paths.select { |row| row.length >= 1 }.flatten
+    target_row = target_row.reverse if negative_movement?(target_row)
+    pieces_present?(target_row)
+  end
+
+  private
+
+  def basic_move?
+    allowed_distance? && same_column?
+  end
+
+  def allowed_distance?
+    starting_row = 1
+    first_move_distance = [1, 2]
+    normal_distance = [1]
+    distance = row(start_square) == starting_row ? first_move_distance : normal_distance
+    distance.include?(row(finish_square) - row(start_square))
+  end
+
+  def capture_allowed?
+    offset_one_row? && offset_one_column? && finish_square.piece_color == "white"
+  end
+
+  def offset_one_row?
+    row(finish_square) - row(start_square) == 1
+  end
+
+  def offset_one_column?
+    (column(start_square) - column(finish_square)).abs == 1
+  end
+
+  def find_file
+    board = gameboard.each_slice(8).to_a.transpose
+    select_row(board)
+  end
+
+  def select_row(board)
+    board.select do |row|
+      [start_square, finish_square].all? { |square| row.include?(square) }
+    end.flatten
+  end
+
+  def pieces_present?(target_row)
+    target_row[target_row.index(start_square) + 1..target_row.index(finish_square)].map(&:piece).all?("-")
+  end
+
+  def negative_movement?(row)
+    row.index(finish_square) < row.index(start_square)
+  end
+end
---
+++ spec/black_pawn_move_spec.rb
@@ -0,0 +1,168 @@
+# frozen_string_literal: true
+
+require_relative "../lib/black_pawn_move"
+
+describe BlackPawnMove do
+  subject(:basic_board) { described_class.new("8/4p3/2p5/8/6p1/1p3P1P/p7/8") }
+
+  describe "#basic_rules?" do
+    # starting position
+    context "pawn moves from e7 to e5" do
+      it "returns true" do
+        basic_board.update_start_square("e7")
+        basic_board.update_finish_square("e5")
+        expect(basic_board.basic_rules?).to eq(true)
+      end
+    end
+
+    context "pawn moves from e7 to e6" do
+      it "returns true" do
+        basic_board.update_start_square("e7")
+        basic_board.update_finish_square("e6")
+        expect(basic_board.basic_rules?).to eq(true)
+      end
+    end
+
+    context "pawn mves from e7 to e5" do
+      it "returns false" do
+        basic_board.update_start_square("e7")
+        basic_board.update_finish_square("e4")
+        expect(basic_board.basic_rules?).to eq(false)
+      end
+    end
+
+    context "pawn mves from e7 to e8" do
+      it "returns false" do
+        basic_board.update_start_square("e7")
+        basic_board.update_finish_square("e8")
+        expect(basic_board.basic_rules?).to eq(false)
+      end
+    end
+
+    context "pawn mves from e7 to g6" do
+      it "returns false" do
+        basic_board.update_start_square("e7")
+        basic_board.update_finish_square("g6")
+        expect(basic_board.basic_rules?).to eq(false)
+      end
+    end
+
+    # normal move
+    context "pawn moves from c6 to c4" do
+      it "returns false" do
+        basic_board.update_start_square("c6")
+        basic_board.update_finish_square("c4")
+        expect(basic_board.basic_rules?).to eq(false)
+      end
+    end
+
+    context "pawn moves from c6 to c5" do
+      it "returns true" do
+        basic_board.update_start_square("c6")
+        basic_board.update_finish_square("c5")
+        expect(basic_board.basic_rules?).to eq(true)
+      end
+    end
+
+    context "pawn moves from c6 to c7" do
+      it "returns false" do
+        basic_board.update_start_square("c6")
+        basic_board.update_finish_square("c7")
+        expect(basic_board.basic_rules?).to eq(false)
+      end
+    end
+
+    context "pawn moves from c6 to a5" do
+      it "returns false" do
+        basic_board.update_start_square("c6")
+        basic_board.update_finish_square("a5")
+        expect(basic_board.basic_rules?).to eq(false)
+      end
+    end
+
+    # capture
+    context "pawn captures from g4 to f3" do
+      it "returns true" do
+        basic_board.update_start_square("g4")
+        basic_board.update_finish_square("f3")
+        expect(basic_board.basic_rules?).to eq(true)
+      end
+    end
+
+    context "pawn captures from g4 to h3" do
+      it "returns true" do
+        basic_board.update_start_square("g4")
+        basic_board.update_finish_square("h3")
+        expect(basic_board.basic_rules?).to eq(true)
+      end
+    end
+
+    context "pawn moves diagonally when no piece to capture" do
+      it "returns false" do
+        basic_board.update_start_square("c6")
+        basic_board.update_finish_square("b5")
+        expect(basic_board.basic_rules?).to eq(false)
+      end
+    end
+
+    context "pawn trys to capture own piece" do
+      it "returns false" do
+        basic_board.update_start_square("b3")
+        basic_board.update_finish_square("a2")
+        expect(basic_board.basic_rules?).to eq(false)
+      end
+    end
+
+    context "pawn moves diagonally backward" do
+      it "returns false" do
+        basic_board.update_start_square("g4")
+        basic_board.update_finish_square("f5")
+        expect(basic_board.basic_rules?).to eq(false)
+      end
+    end
+  end
+
+  describe "#clear_path?" do
+    subject(:pawn_obstructions) { described_class.new("8/4pp1p/p4P2/p3P3/2p5/8/2P5/8") }
+
+    context "pawn move from f7 to f5, white pawn on f6" do
+      it "returns false" do
+        pawn_obstructions.update_start_square("f7")
+        pawn_obstructions.update_finish_square("f5")
+        expect(pawn_obstructions.clear_path?).to eq(false)
+      end
+    end
+
+    context "pawn move from a6 to a5, white pawn on a5" do
+      it "returns false" do
+        pawn_obstructions.update_start_square("a6")
+        pawn_obstructions.update_finish_square("a5")
+        expect(pawn_obstructions.clear_path?).to eq(false)
+      end
+    end
+
+    context "pawn move from e7 to f6" do
+      it "returns true" do
+        pawn_obstructions.update_start_square("e7")
+        pawn_obstructions.update_finish_square("f6")
+        expect(pawn_obstructions.clear_path?).to eq(true)
+      end
+    end
+
+    context "pawn move from h7 to h5, no obstructions" do
+      it "returns true" do
+        pawn_obstructions.update_start_square("h7")
+        pawn_obstructions.update_finish_square("h5")
+        expect(pawn_obstructions.clear_path?).to eq(true)
+      end
+    end
+
+    context "pawn c4-c3, white pawn on c2" do
+      it "returns true" do
+        pawn_obstructions.update_start_square("c4")
+        pawn_obstructions.update_finish_square("c3")
+        expect(pawn_obstructions.clear_path?).to eq(true)
+      end
+    end
+  end
+end
