bb176a86848ac273fcf5995469cae6d0545609be
PlaustralCL <plaustralcl@gmail.com>
2021-11-14 13:58:41 -0500

    Relocated movement classes to a separate folder

---
+++ lib/moves/king_move.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative "piece_move"
+
+# Validates that a requested king move is allowed
+class KingMove < PieceMove
+  def basic_rules?
+    true
+  end
+
+  def clear_path?
+    true
+  end
+end
