3e032e1bd4f3ec1db58258032851a4b5bd57f61f
PlaustralCL <plaustralcl@gmail.com>
2021-11-23 15:04:11 -0500

    Make a board to display

--- lib/display.rb
+++ lib/display.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "board_helper"
+require_relative "color"
 
 # Prepare the display
 class Display
@@ -18,7 +19,52 @@ class Display
   end
 
   def build_display
-    @display_board = gameboard.map { |piece| piece == " - " ? "   " : piece }
-    @display_board = display_board.map { |piece_name| piece_name.center(3) }
+    replace_dashes
+    center_pieces
+    color_squares
+    join_row
+    add_rank_numbers
+    add_file_letters
   end
+
+  def replace_dashes
+    @display_board = gameboard
+    display_board.map { |square| square.piece = " " if square.piece == "-" }
+  end
+
+  def center_pieces
+    display_board.each do |square|
+      square.piece = square.piece.center(3)
+    end
+  end
+
+  def color_squares
+    @display_board = display_board.map do |square|
+      row = square.coordinates.first
+      column = square.coordinates.last
+      piece_name = square.piece
+      if (row.even? && column.even?) || (row.odd? && column.odd?)
+        piece_name.bold_cyan_backgrouond
+      else
+        piece_name.bold
+      end
+    end
+  end
+
+  def join_row
+    @display_board = display_board.flatten.each_slice(8).to_a.map(&:join)
+  end
+
+  def add_rank_numbers
+    @display_board = display_board.map.with_index { |row, index| "#{8 - index}  #{row}\n" }.join
+  end
+
+  def add_file_letters
+    "#{display_board}    a  b  c  d  e  f  g  h"
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  board = Display.new
+  puts board.build_display
 end
